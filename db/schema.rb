e483fdaea561b287c3566d7aedb4767d69161504
kohey <k_kawamura@kiizan-kiizan.co.jp>
2019-03-13 12:56:47 +0900

    部分的に日本語に変換しました

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 20190312073522) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
